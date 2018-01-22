a767c95f8b30c6b523af45b0164c8f43b511d606
demfier <sahu.gaurav719@gmail.com>
2018-01-22 21:06:24 +0530

    Add other query functions in Query class

--- query.py
+++ query.py
@@ -14,6 +14,7 @@ import random
 
 class Query(object):
     """Contains the six type of queries used in PAC"""
+
     def __init__(self, _input):
         self._input = _input
         self.targetHypothesis = []  # Find out what this target should be
@@ -30,6 +31,7 @@ class Query(object):
         if !isinstance(self._input, set):
             print "Input must be a set for the equivalence query"
         else:
+            # _input = targetHypothesis
             if self._input == self.targetHypothesis:
                 return {'bool': True, 'counter_example': None}
             else:
@@ -40,13 +42,40 @@ class Query(object):
                                 self.targetHypothesis)))})
 
     def subset(self, restricted=False):
-        print "Yet to implement"
+        """Will tell if the input is a subset of the target hypothesis"""
+        if !isinstance(self._input, set):
+            print "Input must be a set for the subset query"
+        else:
+            # _input ⊆ targetHypothesis
+            if self._input.issubset(self.targetHypothesis):
+                return {'bool': True, 'counter_example': None}
+            else:
+                return {'bool': False, 'counter_example': random.choice(list(
+                    self._input.difference(self.targetHypothesis)))}
 
     def superset(self, restricted=False):
-        print "Yet to implement"
+        """Will tell if the input is a superset of the target hypothesis"""
+        if !isinstance(self._input, set):
+            print "Input must be a set for the superset query"
+        else:
+            # _input ⊇ targetHypothesis
+            if self._input.issuperset(self.targetHypothesis):
+                return {'bool': True, 'counter_example': None}
+            else:
+                return {'bool': False, 'counter_example': random.choice(list(
+                    self.targetHypothesis.difference(self._input)))}
 
     def disjoint(self, restricted=False):
-        print "Yet to implement"
+        """Will tell if the input is disjoint from target hypothesis"""
+        if !isinstance(self._input, set):
+            print "Input must be a set for the disjointness query"
+        else:
+            # _input ∩ targetHyothesis = ϕ
+            if self._input.intersection(self.targetHypothesis) == set([]):
+                return {'bool': True, 'counter_example': None}
+            else:
+                return {'bool': False, 'counter_example': random.choice(list(
+                    self._input.intersection(self.targetHypothesis)))}
 
     def exhaustive(self, restricted=False):
-        print "Yet to implement"
+        print("`exaustive` method is still to be implemented")
