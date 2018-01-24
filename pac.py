8f61cf3f5d6a1196abf567dc56eefba14efe0851
demfier <sahu.gaurav719@gmail.com>
2018-01-25 00:00:58 +0530

    INitialilze pac

---
+++ pac.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Gaurav Sahu, 23:05 23th January, 2018
+
+Implements PAC class containing methods to be used for PAC-Learning
+For more information, see:
+ - D. Angluin: Queries and Concept Learning, 1988
+ - D. Borchmann, T. Hanika and S. Obiedkov: On the Usability of Probabaly
+   Correct Implication Bases, 2017
+"""
